0c244b3a84fb64b8a13d85824a80e4703aa04eba
0sirion <salvatore.arcuri.9@gmail.com>
2023-04-24 18:13:35 +0200

    s

--- PerfectNumber.py
+++ PerfectNumber.py
@@ -4,7 +4,7 @@ st.header(":red[The Perfect Number]")
 st.subheader(":blue[Give me a number, I'll tell you  if it is perfect number]")
 st.markdown('In number theory, a perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.')
 
-numero = st.slider(label="scorri per scegliere un numero",min_value=0, max_value=100)
+numero = st.slider(label="slide to choose a number:",min_value=0, max_value=100)
 # numero = 6
 
 partial = 0
