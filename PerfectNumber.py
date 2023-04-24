c9220035ecf0506d0b7d47bbf84ba6551f5a8a55
0sirion <salvatore.arcuri.9@gmail.com>
2023-04-24 18:27:25 +0200

    s

--- PerfectNumber.py
+++ PerfectNumber.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.header(":red[The Perfect Number]")
-st.subheader(":blue[Give me a number, I'll tell you  if it is perfect number]")
+st.subheader(":blue[Give me a number, I'll tell you  if it is a perfect number]")
 st.markdown('In number theory, a perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.')
 
 # numero = 6
@@ -11,7 +11,7 @@ partial = 0
 
 def main():
     numero = st.slider (label="slide to choose a number: ", min_value=0, max_value= 100) #on_change=check())
-    
+    numero_input = st.number_input(label="Please enter here an integer")
     
     def perfect():
         partial = 0 
