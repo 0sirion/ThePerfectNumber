0930497ee4c21f46907e91fac8c3fa8bfae58033
0sirion <salvatore.arcuri.9@gmail.com>
2023-04-24 17:44:05 +0200

    streamlit app

---
+++ PerfectNumber.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+# numero = st.slider(label="scorri per scegliere un numero",min_value=0, max_value=100)
+numero = 6
+partial = 0
+
+for i in range(1, numero-1):
+    if numero%i == 0:
+        partial = partial + i
+
+if numero == partial:
+    st.write(numero,"Is a perfect number")
+    
