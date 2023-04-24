63fa2c6f6304adcebb5f1221c8575c97f7480900
0sirion <salvatore.arcuri.9@gmail.com>
2023-04-24 18:24:30 +0200

    s

--- PerfectNumber.py
+++ PerfectNumber.py
@@ -11,14 +11,15 @@ partial = 0
 
 def main():
     numero = st.slider (label="slide to choose a number: ", min_value=0, max_value= 100) #on_change=check())
-
     
-    def perfect(): 
+    
+    def perfect():
+        partial = 0 
         for i in range(1, numero-1):
             if numero%i == 0:
                 partial = partial + i
-                if numero == partial:
-                    st.write(numero,"Is a perfect number")
+        if numero == partial:
+            st.write(numero,"Is a perfect number")
     perfect()
 
 
